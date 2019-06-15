4514d7b3422f6ae1fdecdcd6e8e9e8ad0565ad64
b05901022 <b05901022@ntu.edu.tw>
2019-06-15 17:26:26 +0800

    RVC-Extension

--- DECOMPRESSIONUNIT.v
+++ DECOMPRESSIONUNIT.v
@@ -39,7 +39,7 @@ module DecompressionUnit(
 				decomp_instr[30:27] = 4'b0; //Immediate zero-extention
 				decomp_instr[26]    = orig_instr[5]; //Offset[6]
 				decomp_instr[25]    = orig_instr[12]; //Offset[5]
-				decomp_instr[19:18] = 2'b00; //only x8~x15
+				decomp_instr[19:18] = 2'b01; //only x8~x15
 				decomp_instr[17:15] = orig_instr[9:7]; //x8~x15
 				decomp_instr[14:12] = 3'b010;
 				decomp_instr[6]     = 1'b0;
@@ -72,7 +72,7 @@ module DecompressionUnit(
 						decomp_instr[24:20] = orig_instr[6:2]; //Immediate[4:0]
 						decomp_instr[19:15] = orig_instr[11:7]; //rd, rd=0 if NOP
 						decomp_instr[14:12] = 3'b000;
-						decomp_instr[11:7]  = orig_instr[6:2]; //rd, rd=0 if NOP
+						decomp_instr[11:7]  = orig_instr[11:7]; //rd, rd=0 if NOP
 						decomp_instr[6:2]   = 5'b00100;						
 					end
 					3'b100: begin
@@ -81,11 +81,11 @@ module DecompressionUnit(
 						decomp_instr[29:26] = 4'b0000; //Immediate zero-extension
 						decomp_instr[25]    = orig_instr[12]; //shamt[5] for C.SRAI, C.SRLI; imm[5] for C.ANDI
 						decomp_instr[24:20] = orig_instr[6:2]; //shamt[4:0] for C.SRAI, C.SRLI; imm[4:0] for C.ANDI
-						decomp_instr[19:15] = orig_instr[11:7]; //rd, rd=0 if NOP
+						decomp_instr[19:15] = {2'b01, orig_instr[9:7]}; //rd, rd=0 if NOP
 						decomp_instr[14]    = 1'b1; 
 						decomp_instr[13]    = orig_instr[11]; //1 for C.ANDI; 0 for C.SRAI, C.SRLI
 						decomp_instr[12]    = 1'b1; 
-						decomp_instr[11:7]  = orig_instr[6:2]; //rd, rd=0 if NOP
+						decomp_instr[11:7]  = {2'b01, orig_instr[9:7]}; //rd, rd=0 if NOP
 						decomp_instr[6:2]   = 5'b00100;
 					end
 					3'b001, 3'b101: begin
@@ -100,7 +100,7 @@ module DecompressionUnit(
 						decomp_instr[20]    = orig_instr[12]; //imm[11]
 						decomp_instr[19:12] = 8'b0; //Immediate zero-extension
 						decomp_instr[11:8]  = 4'b0000; //x0 or x1
-						decomp_instr[7]     = orig_instr[15]; //x0 for C.J, x1 for C.JAL
+						decomp_instr[7]     = ~orig_instr[15]; //x0 for C.J, x1 for C.JAL
 						decomp_instr[6:2]   = 5'b11011;
 					end
 					3'b110, 3'b111: begin
@@ -176,110 +176,3 @@ module DecompressionUnit(
 
 endmodule
 
-module InstructionAnalyzer(
-	PC_1,
-	INSTR,
-	PC_add,
-	INSTR_o
-	);
-	
-	// === I/O ===
-	input         PC_1; //PC[1] for checking if now using half block
-	input  [31:0] INSTR; //the whole block fetched from cache/memory
-	output [31:0] PC_add; //addition to PC for next stage
-	output [31:0] INSTR_o; //Instruction send to ID-stage
-
-	// === Wires ===
-	reg  [31:0] INSTR_o;
-	wire [31:0] INSTR_d;
-	reg  [15:0] INSTR_c;
-	reg  [15:0] INSTR_save_r, INSTR_save_w;
-	//reg  [31:0] PC_r, PC_w;
-	reg  [31:0] PC_add;
-	reg         stall_r, stall_w;
-
-	/*
-	INSTR: original instruction(in little endian)
-	INSTR_o: output instruction(in common order)
-	INSTR_c: compressed instruction(in common order)
-	INSTR_d: decompressed instruction(in common order)
-	INSTR_save_r, INSTR_save_w: saved instruction for the next 16-bit for a complete 32-bit instruction(in common order)
-	*/
-
-	/* ------ Current Scheme --------
-
-                   ---------
-                   |CURRENT|
-	               |  P C  |
-	               ---------
-	--------   --------|--------
-			| | 32-bit |        |
-	--------   --------|--------
-	
-	 32-bit instruction
-	<------------------|
-
-	Above condition will need an NOP
-	(Because not enough information
-	for the whole 32-bit instruction)
-	Might be optimized in some way?
-
-	-------------------------------*/
-
-
-	DecompressionUnit DU1(.orig_instr(INSTR_c), .decomp_instr(INSTR_d));
-
-	always@(*) begin
-
-		PC_add       = 0;
-		INSTR_o      = 32'b0;
-		INSTR_c      = 16'b0;
-		INSTR_save_w = 16'b0;
-		stall_w      = 1'b0;
-
-		if ( stall_r ) begin
-			PC_add = 0; //PC_w = PC_r;
-			INSTR_o = { INSTR[23:16], INSTR[31:24], INSTR_save_r };
-			stall_w = 1'b0;
-		end else begin
-			if ( PC_1 ) begin
-				//Half block
-				if ( &INSTR[25:24] ) begin
-					//32-bit instruction
-					PC_add = 4; //PC_w = PC_r + 4;
-					INSTR_o = { INSTR[7:0], INSTR[15:8], INSTR[23:16], INSTR[31:24] };
-				end else begin
-					//16-bit instruction
-					PC_add = 2; //PC_w = PC_r + 2;
-					INSTR_c = { INSTR[23:16], INSTR[31:24] }; //16-bit
-					INSTR_o = INSTR_d;
-				end
-			end else begin
-				//Complete block
-				if ( &INSTR[9:8] ) begin
-					//32-bit instruction
-					PC_add = 4; //PC_w = PC_r + 4;
-					INSTR_o = 32'h00_00_00_13; //NOP
-					stall_w = 1'b1;
-					INSTR_save_w = { INSTR[7:0], INSTR[15:8] }; //Save pre-switched operations					
-				end else begin
-					//16-bit instruction
-					PC_add = 2; //PC_w = PC_r + 2;
-					INSTR_c = { INSTR[7:0], INSTR[15:8] }; //16-bit
-					INSTR_o = INSTR_d;
-				end
-			end
-		end
-	end
-
-	always @(posedge clk or negedge rst_n) begin
-		if(~rst_n) begin
-			INSTR_save_r <= 16'b0;
-			stall_r      <= 1'b0;
-		end else begin
-			INSTR_save_r <= INSTR_save_w;
-			stall_r      <= stall_w;
-		end
-	end
-
-endmodule
--- cache_dm_ro.v
+++ cache_dm_ro.v
@@ -188,3 +188,164 @@ module cache_ro(
 	    end
 	end
 endmodule
+
+//read-only cache for compression instructions
+module cache_comp(
+    clk,
+    proc_reset,
+    proc_addr,
+    proc_rdata,
+    proc_stall,
+    mem_read,
+    mem_addr,
+    mem_rdata,
+    mem_ready
+	);
+
+	//==== parameters definition ==============================
+    // for FSM state...
+    localparam START    = 1'b0;
+    localparam ALLOCATE = 1'b1;
+
+    // for loop
+    integer i;
+
+	//==== input/output definition ============================
+    input          clk;
+    // processor interface
+    input          proc_reset;
+    input   [30:0] proc_addr;
+    output         proc_stall;
+    output  [31:0] proc_rdata;
+    // memory interface
+    input  [127:0] mem_rdata;
+    input          mem_ready;
+    output  reg    mem_read; 
+    output  reg [27:0] mem_addr;
+
+	//==== wire/reg definition ================================
+    //for storage
+    reg         valid_w[0:7];
+    reg         valid_r[0:7];
+    reg  [24:0] tag_w  [0:7];
+    reg  [24:0] tag_r  [0:7];
+    reg  [15:0] word_w[0:63]; 
+    reg  [15:0] word_r[0:63]; 
+
+    // for FSM
+    reg  state_nxt, state;  
+
+    // hit or miss
+    reg     hit_or_miss; // 1 for hit, 0 for miss
+
+    // for circuit output
+    reg [31:0]  rdata;
+    reg         stall;
+
+	//==== Finite State Machine ===============================
+	always@( posedge clk) begin
+	    if( proc_reset ) begin
+	        state <= START;
+	    end else begin
+	        state <= state_nxt;
+	    end
+	end
+
+	//==== next state logic =====================================
+	always@(*) begin
+	    case ( state )
+	        START:
+	            begin 
+	                if( hit_or_miss ) state_nxt = START; // hit!!
+	                else state_nxt = ALLOCATE;
+	            end
+	        ALLOCATE:
+	            begin
+	                if( mem_ready ) state_nxt = START;
+	                else state_nxt = ALLOCATE;
+	            end
+	    endcase
+	end
+
+	//==== combinational circuit ==============================
+
+	assign proc_rdata = rdata;
+	assign proc_stall = stall;
+
+	always@(*) begin
+	    //==== Default value ==================================
+	    rdata          = 32'b0;
+	    stall          = 1'b0;
+	    mem_read       = 0;
+	    mem_addr       = proc_addr[29:2];
+	    hit_or_miss    = 1'b0;
+	    for (i=0;i<8;i=i+1) begin
+	        valid_w[i] = valid_r[i]; 
+	        tag_w[i]   = tag_r[i]; 
+	    end
+	    for (i=0;i<64;i=i+1) begin
+	        word_w[i]  = word_r[i]; 
+	    end
+	    //=====================================================
+	    case ( state ) 
+	        START:
+	            begin
+	                if( valid_r[proc_addr[5:3]] && ( tag_r[proc_addr[5:3]] == proc_addr[30:6] ) ) begin
+	                	//20190613 1802
+	                    // hit
+	                    stall = 1'b0;
+	                    hit_or_miss = 1'b1;
+	                    rdata = word_r[proc_addr[4:0]]; //always read
+	                end
+	                else begin
+	                    // miss
+	                    stall = 1'b1;
+	                end
+	            end
+	        ALLOCATE:
+	            begin
+	                stall = 1'b1;
+	                if ( mem_ready ) begin
+	                    case ( proc_addr[4:2] ) 
+	                        3'd0: tag_w[0] = proc_addr[29:5];
+	                        3'd1: tag_w[1] = proc_addr[29:5];
+	                        3'd2: tag_w[2] = proc_addr[29:5];
+	                        3'd3: tag_w[3] = proc_addr[29:5];
+	                        3'd4: tag_w[4] = proc_addr[29:5];
+	                        3'd5: tag_w[5] = proc_addr[29:5];
+	                        3'd6: tag_w[6] = proc_addr[29:5];
+	                        3'd7: tag_w[7] = proc_addr[29:5];
+	                    endcase
+	                    valid_w[proc_addr[4:2]] = 1'b1;
+	                    {{word_w[{proc_addr[4:2], 2'b11}]}, {word_w[{proc_addr[4:2], 2'b10}]},
+	                     {word_w[{proc_addr[4:2], 2'b01}]}, {word_w[{proc_addr[4:2], 2'b00}]}} = mem_rdata;
+	                end
+	                else begin
+	                    mem_addr = proc_addr[29:2];
+	                    mem_read =  1'b1;
+	                end
+	            end
+	    endcase
+	end
+	//==== sequential circuit =================================
+	always@( posedge clk ) begin
+	    if( proc_reset ) begin
+	        for (i=0;i<8;i=i+1) begin
+	            valid_r[i] <= 1'b0; // reset valid bit
+	            tag_r[i]   <= 25'b0; // reset tag
+	        end
+	        for (i=0;i<32;i=i+1) begin
+	            word_r[i]  <= 32'b0; // reset words
+	        end
+	    end
+	    else begin
+	        for (i=0;i<8;i=i+1) begin
+	            valid_r[i] <= valid_w[i]; // reset valid bit
+	            tag_r[i]   <= tag_w[i]; // reset tag
+	        end
+	        for (i=0;i<32;i=i+1) begin
+	            word_r[i]  <= word_w[i]; // reset words
+	        end
+	    end
+	end
+endmodule
