8ad0c5364d0d5f5e5f3684eedd2317df7d19bcbd
b05901022 <b05901022@ntu.edu.tw>
2019-06-19 23:02:22 +0800

    RVC-Extension-pass

--- RVC_Extension/FORWARDING_UNIT.v
+++ RVC_Extension/FORWARDING_UNIT.v
@@ -18,41 +18,36 @@ module FORWARDING_UNIT(
 	wire FLAG_RS1_ZERO, FLAG_RS2_ZERO;
 
 
-	assign FLAG1 = (EXMEM_RD == IDEX_RS1);
-	assign FLAG2 = (MEMWB_RD == IDEX_RS1);
-	assign FLAG3 = (EXMEM_RD == IDEX_RS2);
-	assign FLAG4 = (MEMWB_RD == IDEX_RS2);
+	assign FLAG1 = ~|(EXMEM_RD ^ IDEX_RS1);
+	assign FLAG2 = ~|(MEMWB_RD ^ IDEX_RS1);
+	assign FLAG3 = ~|(EXMEM_RD ^ IDEX_RS2);
+	assign FLAG4 = ~|(MEMWB_RD ^ IDEX_RS2);
 
-	assign FLAG5 = (EXMEM_RD == IFID_RS1);
-	assign FLAG6 = (MEMWB_RD == IFID_RS1);
-	assign FLAG7 = (EXMEM_RD == IFID_RS2);
-	assign FLAG8 = (MEMWB_RD == IFID_RS2);
+	assign FLAG5 = ~|(EXMEM_RD ^ IFID_RS1);
+	assign FLAG6 = ~|(MEMWB_RD ^ IFID_RS1);
+	assign FLAG7 = ~|(EXMEM_RD ^ IFID_RS2);
+	assign FLAG8 = ~|(MEMWB_RD ^ IFID_RS2);
 
-	assign FLAG_RS1_ZERO = (IFID_RS1 == 0)? 1'b1: 1'b0;
-	assign FLAG_RS2_ZERO = (IFID_RS2 == 0)? 1'b1: 1'b0;
+	assign FLAG_RS1_ZERO = (|(IFID_RS1))? 1'b1: 1'b0;
+	assign FLAG_RS2_ZERO = (|(IFID_RS2))? 1'b1: 1'b0;
+
+	assign FLAG_RS1_ZERO_EX = (|(IDEX_RS1))? 1'b1: 1'b0;
+	assign FLAG_RS2_ZERO_EX = (|(IDEX_RS2))? 1'b1: 1'b0;
 
 	always@(*) begin
-		FORWARD_A_ex[1] = EXMEM_RegWrite && FLAG1;
-		FORWARD_A_ex[0] = MEMWB_RegWrite && FLAG2;
+		FORWARD_A_ex[1] = (EXMEM_RegWrite && FLAG1 && (FLAG_RS1_ZERO_EX));
+		FORWARD_A_ex[0] = (MEMWB_RegWrite && FLAG2 && (FLAG_RS1_ZERO_EX));
 
-		FORWARD_B_ex[1] = EXMEM_RegWrite && FLAG3;
-		FORWARD_B_ex[0] = MEMWB_RegWrite && FLAG4;
+		FORWARD_B_ex[1] = (EXMEM_RegWrite && FLAG3 && (FLAG_RS2_ZERO_EX));
+		FORWARD_B_ex[0] = (MEMWB_RegWrite && FLAG4 && (FLAG_RS2_ZERO_EX));
 		
-		if (~FLAG_RS1_ZERO) begin
-			FORWARD_A_id[1] = EXMEM_RegWrite && FLAG5;
-			FORWARD_A_id[0] = MEMWB_RegWrite && FLAG6;
-		end
-		else begin
-			FORWARD_A_id = 2'b00;
-		end
+		
+		FORWARD_A_id[1] = (EXMEM_RegWrite && FLAG5 && (FLAG_RS1_ZERO));
+		FORWARD_A_id[0] = (MEMWB_RegWrite && FLAG6 && (FLAG_RS1_ZERO));
+		
 
-		if (~FLAG_RS2_ZERO) begin
-			FORWARD_B_id[1] = EXMEM_RegWrite && FLAG7;
-			FORWARD_B_id[0] = MEMWB_RegWrite && FLAG8;
-		end
-		else begin
-			FORWARD_B_id = 2'b00;
-		end
+		FORWARD_B_id[1] = (EXMEM_RegWrite && FLAG7 && (FLAG_RS2_ZERO));
+		FORWARD_B_id[0] = (MEMWB_RegWrite && FLAG8 && (FLAG_RS2_ZERO));
 	end
 
 endmodule
