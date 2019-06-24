f2b5b865b47c5218b33a4f9810bd34aef42cf54e
GGEEDER <32541746+GGEEDER@users.noreply.github.com>
2019-06-24 14:23:53 +0800

    Add files via upload

---
+++ final_version/HAZARD_DETECTION_UNIT.v
@@ -0,0 +1,29 @@
+module HAZARD_DETECTION_UNIT(
+	input [4:0] IDEX_RT,
+	input [4:0] IFID_RS,
+	input [4:0] IFID_RT,
+	input       MemRead,
+	output  reg    stall
+	);
+	
+	/*
+	if (stall) begin
+		PCWrite = 1'b0;
+		IF_ID_Write = 1'b0;
+		Ctrl_Sel = 1'b0;
+	end else begin
+		PCWrite = 1'b1;
+		IF_ID_Write = 1'b1;
+		Ctrl_Sel = 1'b1;
+	end
+	*/
+	//reg stall;
+	always@(*) begin
+		if (MemRead && ( (IDEX_RT == IFID_RS) | (IDEX_RT == IFID_RT) ) ) begin
+			stall = 1'b1;
+		end else begin
+			stall = 1'b0;
+		end
+	end
+
+endmodule
